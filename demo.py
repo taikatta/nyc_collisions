dd8d3d724df929ce750f075c87d766cf060b1f88
Boglarka Barabas <taikatta@gmail.com>
2020-05-30 11:44:54 +0100

    URL changed

--- demo.py
+++ demo.py
@@ -6,7 +6,7 @@ import plotly.express as px
 
 DATE_TIME = "date/time"
 DATA_URL = (
-    "Motor_Vehicle_Collisions_-_Crashes.csv" #"/home/cicada/Downloads/rhyme/streamlit/Motor_Vehicle_Collisions_-_Crashes.csv
+    "https://nyc-collisions.s3-eu-west-1.amazonaws.com/Motor_Vehicle_Collisions_-_Crashes.csv" #"/home/cicada/Downloads/rhyme/streamlit/Motor_Vehicle_Collisions_-_Crashes.csv
 )
 
 st.title("Motor Vehicle Collisions in New York City")
